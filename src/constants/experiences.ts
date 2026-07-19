3d23be18c51fb1f3b94de869b1366a2168757035
Robert Daniel Eavorschi <robertdaniel.eavorschi@sistinf.it>
2026-07-19 10:19:24 +0200

    feat(istruzione): add istruzione section

--- src/constants/experiences.ts
+++ src/constants/experiences.ts
@@ -8,7 +8,7 @@ export const experiences: ExperienceLanguageView[] = [
                 title: 'Sistemi Informativi',
                 fromDate: 'Dic 2023',
                 toDate: 'Presente',
-                imageUrl: 'public/sistemi.jpeg',
+                imageUrl: 'sistemi.jpeg',
                 tech: [
                     'Java 8/21', 'Spring Boot 3/4',
                     'OracleDB', 'PostgreSQL', 'MongoDB',
